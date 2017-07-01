340abd2c2c373370e22f39fbc0bd1a93aa2c1618
Eric Ma <ericmajinglong@gmail.com>
2017-07-01 14:54:44 -0400

    pep8 fixes

--- app.py
+++ app.py
@@ -237,5 +237,6 @@ def sync():
                            inactive=inactive,
                            summary=members_summary())
 
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5050)
