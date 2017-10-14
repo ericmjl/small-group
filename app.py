76cc46c9a65a0321cba0995c58ecffab0d00df56
Eric Ma <ericmajinglong@gmail.com>
2017-10-14 19:03:20 -0400

    Started bootstrap4 changes

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ import os.path as op
 
 from collections import Counter, defaultdict
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 from member import Member
 
 from smallgroup import SmallGroup
 
 from tinydb import Query, TinyDB
+
+from bokeh.models import ColumnDataSource
+from bokeh.plotting import figure
+from bokeh.embed import components
 
 # Initial checks
 # 1. Make sure there is a ".smallgroup/" directory under the home dir.
@@ -26,6 +30,7 @@ os.system('bash sync.sh')
 app = Flask(__name__)
 db = TinyDB(db_path)
 
+GENDERS = ['M', 'F']
 FAITH_STATUSES = ['baptized', 'believer', 'seeker', 'unknown']
 ROLES = ['counselor', 'facilitator', 'none']
 MEMBER_SIGNATURE = ['name', 'surname', 'gender', 'faith_status', 'role',
@@ -35,13 +40,58 @@ MEMBER_SIGNATURE = ['name', 'surname', 'gender', 'faith_status', 'role',
 def members_summary():
     """
     A helper function that summarizes the members' data.
+
+    Returns the set of bokeh plots to show on the main interface.
     """
     summary = defaultdict(Counter)
+    fields_of_interest = ['gender', 'faith_status', 'role']
     for m in db.all():
         if m['active'] == 'true':
-            for s in MEMBER_SIGNATURE[2:-2]:
+            for s in fields_of_interest:
                 summary[s][m[s]] += 1
-    return summary
+    print(summary)
+
+    genders = {'xlabels': GENDERS}
+    genders['data'] = []
+    for g in GENDERS:
+        genders['data'].append(summary['gender'][g])
+    gdata = ColumnDataSource(genders)
+
+    faith_statuses = {'xlabels': FAITH_STATUSES}
+    faith_statuses['data'] = []
+    for f in FAITH_STATUSES:
+        faith_statuses['data'].append(summary['faith_status'][f])
+    fdata = ColumnDataSource(faith_statuses)
+
+    roles = {'xlabels': ROLES}
+    roles['data'] = []
+    for r in ROLES:
+        roles['data'].append(summary['role'][r])
+    rdata = ColumnDataSource(roles)
+    print(roles)
+
+    pg = figure(x_range=genders['xlabels'], plot_height=250, plot_width=250)
+    pg.vbar(x='xlabels', source=gdata, width=0.4, top='data')
+    pg_script, pg_div = components(pg)
+
+    pf = figure(x_range=faith_statuses['xlabels'], plot_height=250,
+                plot_width=250)
+    pf.vbar(x='xlabels', source=fdata, width=0.4, top='data')
+    pf_script, pf_div = components(pf)
+
+    pr = figure(x_range=roles['xlabels'], plot_height=250, plot_width=250)
+    pr.vbar(x='xlabels', top='data', source=rdata, width=0.4)
+    pr_script, pr_div = components(pr)
+
+    bk = defaultdict(dict)
+    bk['pg']['script'] = pg_script
+    bk['pg']['div'] = pg_div
+    bk['pr']['script'] = pr_script
+    bk['pr']['div'] = pr_div
+    bk['pf']['script'] = pf_script
+    bk['pf']['div'] = pf_div
+
+    return bk
 
 
 def split_members_by_active():
@@ -58,12 +108,12 @@ def main():
     The main page. Lists all the group members.
     """
     active, inactive = split_members_by_active()
-
+    bokehplots = members_summary()
     return render_template('index.html',
                            all_members=db.all(),
                            active=active,
                            inactive=inactive,
-                           summary=members_summary())
+                           bokehplots=bokehplots)
 
 
 @app.route('/archive/<int:id>', methods=['POST'])
@@ -72,12 +122,7 @@ def archive(id):
     Archives the member.
     """
     db.update({'active': 'false'}, eids=[id])
-    active, inactive = split_members_by_active()
-    return render_template('index.html',
-                           all_members=db.all(),
-                           active=active,
-                           inactive=inactive,
-                           summary=members_summary())
+    return redirect('/')
 
 
 @app.route('/activate/<int:id>', methods=['POST'])
@@ -86,12 +131,7 @@ def activate(id):
     Activates the member.
     """
     db.update({'active': 'true'}, eids=[id])
-    active, inactive = split_members_by_active()
-    return render_template('index.html',
-                           all_members=db.all(),
-                           active=active,
-                           inactive=inactive,
-                           summary=members_summary())
+    return redirect('/')
 
 
 @app.route('/view_member/<int:id>', methods=['POST'])
@@ -106,23 +146,15 @@ def view_member(id):
 @app.route('/update_member/<int:id>', methods=['POST'])
 def update_member(id):
     for k, v in request.form.items():
-        # print(k, v)
         db.update({k: v}, eids=[id])
-
-    active, inactive = split_members_by_active()
-    return render_template('index.html',
-                           all_members=db.all(),
-                           active=active,
-                           inactive=inactive,
-                           summary=members_summary())
+    return redirect('/')
 
 
-@app.route('/add', methods=['POST'])
+@app.route('/add')
 def add():
     """
     The page used to add one member to the group.
     """
-
     return render_template('form.html',
                            member=None,
                            faith_statuses=FAITH_STATUSES,
@@ -134,18 +166,11 @@ def add_member():
     """
     Adds a member to the database.
     """
-    # Add the new member to the database
     data = dict()
     for s in MEMBER_SIGNATURE:
         data[s] = request.form[s]
     db.insert(data)
-
-    active, inactive = split_members_by_active()
-    return render_template('index.html',
-                           all_members=db.all(),
-                           active=active,
-                           inactive=inactive,
-                           summary=members_summary())
+    return redirect('/')
 
 
 @app.route('/delete/<int:id>', methods=['POST'])
@@ -154,12 +179,7 @@ def delete(id):
     Deletes a member from the database.
     """
     db.remove(eids=[id])
-    active, inactive = split_members_by_active()
-    return render_template('index.html',
-                           all_members=db.all(),
-                           active=active,
-                           inactive=inactive,
-                           summary=members_summary())
+    return redirect('/')
 
 
 def has_one_believer(members):
@@ -234,12 +254,7 @@ def sync():
     Forces a sync of the database with GitHub.
     """
     os.system('bash sync.sh')
-    active, inactive = split_members_by_active()
-    return render_template('index.html',
-                           all_members=db.all(),
-                           active=active,
-                           inactive=inactive,
-                           summary=members_summary())
+    return redirect('/')
 
 
 if __name__ == '__main__':
